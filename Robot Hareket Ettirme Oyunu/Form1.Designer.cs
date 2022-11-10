0fe3a6a9822dfaf7a795dd07d3e014c124ddae56
Eren <117864036+Elcieren@users.noreply.github.com>
2022-11-10 12:14:54 +0300

    Robot Move Game Algorithm
    Kullanıcıdan konum ve alan bilgisi alan .
    Bu bilgiler ışığında konuma ilerleyen
    ve konuma vardıktan sonra gerekli adımları gösteren oyun

---
+++ Robot Hareket Ettirme Oyunu/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace Deneme_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Location = new System.Drawing.Point(18, 109);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(910, 429);
+            this.panel1.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(793, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 18);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "KONUM ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(613, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(174, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Hareket Sonrası Konum:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hareket Ettir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Komutlar";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 56);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(201, 26);
+            this.textBox4.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 111);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(572, 279);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(625, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 26);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(395, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 26);
+            this.textBox3.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(171, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 26);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 26);
+            this.textBox1.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(796, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 40);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Konumlandır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(622, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 18);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Yönü";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(392, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 18);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Y Eksenindeki Konum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "X eksenindeki Konum";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Alan Boyutu";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(979, 126);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(246, 154);
+            this.panel2.TabIndex = 20;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(979, 286);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 21;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(979, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 38);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Çiz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1253, 550);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Robot Hareket Ettirme Oyunu/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Deneme_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string komut_201104078; 
+        string Yonler_201104078;
+        int AlanBoyut_201104078, xEksen_201104078, yEksen_201104078;
+        int M_harfi_sayisi_201104078 = 0;
+        
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            komut_201104078 = textBox4.Text; // textboxtan alan boyutunu alup en genelde tanımladığım string ifadeye atadım 
+            int KomutUzaklık_201104078 = komut_201104078.Length; //atamış olduğum string ifadenin uzunluğunu aldım 
+            char[] karakterler = komut_201104078.ToCharArray(); // almış olduğum string ifadenin char olarak diziye atadım
+            foreach (char karakter in karakterler) // foreach kullandım çünkü o string ifadeyi içinden kaç tane M harfi olduğunu çekmem gerekiyordu en kolay yolda diziden rahatça ulaşbilmekti
+            {
+                if (karakter == 'M')
+                {
+                    M_harfi_sayisi_201104078++;
+
+                }
+            }
+            for (int j = 0; j < M_harfi_sayisi_201104078; j++) // oluşturduğum algoritmada for döngüsüne sokarak m harfine kadar dönmesini sağladım 
+            {
+                for (int i = 0; i < KomutUzaklık_201104078; i++)
+                {
+                    if (Yonler_201104078 == "Kuzey")// verilen komutun ilk önce girilmesi gereken if değeri için her bir yön için başlangıç if oluşturdum 
+                    {
+                        if (komut_201104078[i] == 'L') // kağıt üzerinde uzun uğraşlar sonra matematiksel olarak her yöne girdikten sonra o yönün işlevinin vereilen left right konutları için X ve Y için hareketlerini inceledim ve kod kısmına aktardım
+                            Yonler_201104078 = "Batı";// örneğin kuzey komutunda verilen yön left ise yönü batı oluyor ardından move komutu bu işlem sonrası Y ekseni atışa geçiyor
+                        else if (komut_201104078[i] == 'R')
+                            Yonler_201104078 = "Doğu";
+                        else if (komut_201104078[i] == 'M')
+                        {
+                            j++; // bu arttırım üstteki for döngüsü içerisinde hareket sırası kadar arttırım yapmakta
+                            yEksen_201104078++; // y eksenini arttırıyor
+                            string[] RW = { j.ToString(), xEksen_201104078.ToString(), yEksen_201104078.ToString() };// listview hareket adımları ve x , y konumunu yazdrıdım 
+                            var satir1 = new ListViewItem(RW);
+                            listView1.Items.Add(satir1);
+                            label7.Text = xEksen_201104078.ToString() + "- " + yEksen_201104078.ToString() + "\n"; // son konumu x ve y nin
+                        }
+                    }
+                    else if (Yonler_201104078 == "Güney") // diğer bir else if ifadeside verilen komutta eğer iç if güney olursa bura girip hx ve y nin hareket durumu için incelenmesini sağlıcak
+                    {
+                        if (komut_201104078[i] == 'L')
+                            Yonler_201104078 = "Doğu";
+                        else if (komut_201104078[i] == 'R')
+                            Yonler_201104078 = "Batı";
+                        else if (komut_201104078[i] == 'M')
+                        {
+                            j++;// bu arttırım üstteki for döngüsü içerisinde hareket sırası kadar arttırım yapmakta
+                            yEksen_201104078--;// y eksenini azaltıyor
+                            
+                            string[] RW1 = { j.ToString(), xEksen_201104078.ToString(), yEksen_201104078.ToString() };// listview hareket adımları ve x , y konumunu yazdrıdım 
+                            var satir2 = new ListViewItem(RW1);
+                            listView1.Items.Add(satir2);
+                            label7.Text = xEksen_201104078.ToString() + "- " + yEksen_201104078.ToString() + "\n";
+                        }
+                    }
+                    else if (Yonler_201104078 == "Batı") // örneğin güney komutunu alır ve right komutu ardıdan gelirse yön else if batıya giricek ve move kontrolu ile x eksenini bir bir azaltcak 
+                    {
+                        if (komut_201104078[i] == 'L')
+                            Yonler_201104078 = "Güney";
+                        else if (komut_201104078[i] == 'R')
+                            Yonler_201104078 = "Kuzey";
+                        else if (komut_201104078[i] == 'M')
+                        {
+                            j++;// bu arttırım üstteki for döngüsü içerisinde hareket sırası kadar arttırım yapmakta
+                            xEksen_201104078--; // x ekseni değerin azaltıyo
+                           
+                            string[] RW2 = { j.ToString(), xEksen_201104078.ToString(), yEksen_201104078.ToString() };// listview hareket adımları ve x , y konumunu yazdrıdım 
+                            var satir3 = new ListViewItem(RW2);
+                            listView1.Items.Add(satir3);
+                            label7.Text += xEksen_201104078.ToString() + "- " + yEksen_201104078.ToString() + "\n";
+                        }
+                    }
+                    else if (Yonler_201104078 == "Doğu")
+                    {
+                        if (komut_201104078[i] == 'L')
+                            Yonler_201104078 = "Kuzey";
+                        else if (komut_201104078[i] == 'R')
+                            Yonler_201104078 = "Güney";
+                        else if (komut_201104078[i] == 'M')
+                        {
+                            j++;// bu arttırım üstteki for döngüsü içerisinde hareket sırası kadar arttırım yapmakta
+                            xEksen_201104078++; // x değerini arttırıyor
+                            
+                            string[] RW3 = { j.ToString(), xEksen_201104078.ToString(), yEksen_201104078.ToString() };// listview hareket adımları ve x , y konumunu yazdrıdım 
+                            var satir4 = new ListViewItem(RW3);
+                            listView1.Items.Add(satir4);
+                            label7.Text = xEksen_201104078.ToString() + "- " + yEksen_201104078.ToString() + "\n"; // son konumu x ve y nin
+                        }
+                    }
+                }
+                
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) // bu kısım algoritma mantığını oluştırmama çok yardım etti ardından kağıt kısmına geçince az çok oluştu 
+        {
+            Graphics gr = panel2.CreateGraphics();
+            Pen mypen = new Pen(Brushes.Black, 1);
+            int LİNES = Convert.ToInt32(textBox5.Text);
+            Font myfont = new Font("Arial", 10);
+            float x = 0;
+            float y = 0;
+            float xspace = panel2.Width / LİNES;
+            float yspace = panel2.Height / LİNES;
+            // Dikey Çizgi
+            for (int i = 0; i < LİNES + 1; i++)
+            {
+                gr.DrawLine(mypen, x, y, x, panel2.Height);
+                x += xspace;
+            }
+            x = 0;
+            // Yatay Çizgi
+            for (int i = 0; i < LİNES + 1; i++)
+            {
+                gr.DrawLine(mypen, x, y, panel2.Width, y);
+                y += yspace;
+            }
+            x = 0;
+            y = 0;
+            int counter = 1;
+            for (int i = 0; i < LİNES; i++)
+            {
+                for (int j = 0; j < LİNES; j++)
+                {
+                    gr.DrawString(Convert.ToString(counter), myfont, Brushes.Black, x, y);
+                    x += xspace;
+                    counter++;
+                }
+                y += yspace;
+                x = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] yönler = { "Kuzey", "Güney", "Doğu", "Batı" }; // combobox için bir dizi atadım 
+            comboBox1.Items.AddRange(yönler); // diziyi combobox ekledim 
+
+            listView1.Columns.Add("Hareket Sırası", 150);// Listviwe kod yazarak tanımladım boyutuda elle girdim 
+            listView1.Columns.Add("X Konumu", 100);
+            listView1.Columns.Add("Y konumu", 100);
+            listView1.View = View.Details; // listview aktif ettim 
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int max;
+            AlanBoyut_201104078 = Convert.ToInt32(textBox1.Text); // alan boyutunu aldım 
+            max = AlanBoyut_201104078;
+            xEksen_201104078 = Convert.ToInt32(textBox2.Text);//x eksenini aldım 
+            yEksen_201104078 = Convert.ToInt32(textBox3.Text);// y eksenini aldım 
+            if (xEksen_201104078 >= max ) // girilen boyuttan fazla x değeri girilmemesi için 
+            {
+                MessageBox.Show("X KONUMUNU  BOYUTUNDAN BÜYÜK GİREMESSİN","UYARI!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                textBox2.Clear();
+                
+            }
+            else if (yEksen_201104078 > max)// girilen boyuttan fazla y değeri girilmemesi için 
+            {
+                MessageBox.Show("Y KONUMUNU  BOYUTUNDAN BÜYÜK GİREMESSİN", "UYARI!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox3.Clear();
+            }
+            Yonler_201104078 = comboBox1.Text;
+            MessageBox.Show("Yön Bilgileri Alındı Lütfen Komut Giriniz", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information); // alındığına dair kullanıcıya bilgi verdim 
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            comboBox1.Text = "";
+            textBox4.Focus();
+
+
+
+
+
+
+        }
+    }
+}
